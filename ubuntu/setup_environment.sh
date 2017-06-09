e2813892f72b0d74508848af7c04388c2141d1d3
Vladimir Petrigo <taenaru@gmail.com>
2017-06-09 17:56:04 +0300

    Create setup_environment.sh

--- ubuntu/setup_environment.sh
+++ ubuntu/setup_environment.sh
@@ -6,9 +6,12 @@ apt-get upgrade -y -q
 apt-get install aptitude git gcc g++ clang make binutils build-essential \
     python3 python3-pip tmux wget curl
 
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
+USER=$(who | head -1 | awk '{ print $1; }')
 
-git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-~/.vim_runtime/install_awesome_vimrc.sh
+git clone --depth=1 https://github.com/Bash-it/bash-it.git /home/$USER/.bash_it
+chown -R $USER:$USER /home/$USER/.bash_it
+sudo -H -u $USER /home/$USER/.bash_it/install.sh
 
+git clone https://github.com/amix/vimrc.git /home/$USER/.vim_runtime
+chown -R $USER:$USER /home/$USER/.vim_runtime
+sudo -H -u $USER sh /home/$USER/.vim_runtime/install_awesome_vimrc.sh
