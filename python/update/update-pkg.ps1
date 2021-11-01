ec3b1187160df5ae4f02418cd268a17ad3197904
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2021-11-01 13:42:24 +0300

    Update update-pkg.ps1

--- python/update/update-pkg.ps1
+++ python/update/update-pkg.ps1
@@ -1,1 +1,3 @@
-pip list -o | select -skip 2 | foreach-object { "$_" -split " " | select -First 1 } | foreach { pip install -U $_ }
+pip list -o | select -skip 2 | foreach { pip install -U ($_ -split '\s+')[0] }
+# In the case of working with specific Python installation the following stuff can be used:
+# py -3.10 -m pip list -o | select -skip 2 | foreach { py -3.10 -m pip install -U ($_ -split '\s+')[0] }
